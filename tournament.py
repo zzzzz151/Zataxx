06ec1c574e6a1d35d6a95e1a36fdd0bd4c33ffb5
zzzzz <ricardopinto15@hotmail.com>
2023-12-05 12:06:12 +0000

    .

--- tournament.py
+++ tournament.py
@@ -8,205 +8,21 @@ import ataxx
 import random
 import time
 import signal
+import selectors
 from sprt import *
 
-def handle_ctrl_c(subprocesses):
-    print("Ctrl+C pressed. Terminating engines...")
-    for subprocess in subprocesses:
-        subprocess.terminate()
-    print("Engines terminated.")
-    exit(1)
+# Constants
+GAME_MILLISECONDS = 10000
+GAME_INCREMENT_MILLISECONDS = 100
+GAME_RESULT_NORMAL = 0
+GAME_RESULT_OUT_OF_TIME = 1
 
-def run_game(engine1, engine2):
-    engine1.send_command_read_response("uainewgame")
-    engine2.send_command_read_response("uainewgame")
-
-    fen_opening = OPENINGS[random.randint(0, len(OPENINGS) - 1)].strip()
-    board = ataxx.Board(fen_opening)
-    fen_opening = fen_opening.replace("x", "r").replace("o", "b").strip()
-
-    # randomly decide engine color, openings_3ply.txt is 3 plies so always blue to move
-    if random.randint(0,1) == 0:
-        engine1.color = RED
-        engine2.color = BLUE
-        engine_to_play = engine2
-    else:
-        engine1.color = BLUE
-        engine2.color = RED
-        engine_to_play = engine1
-
-    engine1.milliseconds_left = engine2.milliseconds_left = GAME_MILLISECONDS
-
-    while not board.gameover():
-        fen = board.get_fen().replace("x", "r").replace("o", "b").strip()
-        engine1.send_command_read_response("position fen " + fen)
-        engine2.send_command_read_response("position fen " + fen)
-
-        if engine1.color == RED:
-            red_milliseconds_left = engine1.milliseconds_left
-            blue_milliseconds_left = engine2.milliseconds_left
-        else:
-            red_milliseconds_left = engine2.milliseconds_left
-            blue_milliseconds_left = engine1.milliseconds_left
-
-        command = "go rtime " + str(red_milliseconds_left) + " btime " + str(blue_milliseconds_left)
-        command += " rinc " + str(GAME_INCREMENT_MILLISECONDS) + " binc " + str(GAME_INCREMENT_MILLISECONDS)
-
-        turn_start_time = time.time()
-        str_move = engine_to_play.send_command_read_response(command).split(" ")[1]
-
-        # subtract time spent this turn
-        engine_to_play.milliseconds_left -= int((time.time() - turn_start_time) * 1000)
-
-        if engine_to_play.milliseconds_left <= 0:
-            return (ENGINE1_LOSS, ENGINE1_OUT_OF_TIME) if engine1 == engine_to_play else (ENGINE2_LOSS, ENGINE2_OUT_OF_TIME)
-
-        board.makemove(ataxx.Move.from_san(str_move))
-        engine_to_play = engine1 if engine_to_play == engine2 else engine2        
-        engine1.milliseconds_left += GAME_INCREMENT_MILLISECONDS
-        engine2.milliseconds_left += GAME_INCREMENT_MILLISECONDS
-
-    str_result = board.result().strip()
-    if str_result == "1-0":
-        return ENGINE1_WIN if engine1.color == RED else ENGINE1_LOSS, GAME_RESULT_NORMAL
-    elif str_result == "0-1":
-        return ENGINE1_WIN if engine1.color == BLUE else ENGINE1_LOSS, GAME_RESULT_NORMAL
-    return DRAW, GAME_RESULT_NORMAL
-
-class Game:
-    board = None
-    engine1 = None
-    engine2 = None
-    name1 = "No name"
-    name2 = "No name"
-    engine_red = None
-    engine_blue = None
-    milliseconds_red = None
-    milliseconds_blue = None
-    GAME_MILLISECONDS = 10000
-    GAME_INCREMENT_MILLISECONDS = 100
-
-    def __init__(self, exe1, exe2):
-        self.engine1 = subprocess.Popen(exe1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-        self.engine2 = subprocess.Popen(exe2, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-
-        # Send "uai" and get name of each engine
-        for eng in [self.engine1, self.engine2]:
-            subprocess.stdin.write("uai\n")
-            subprocess.stdin.flush()
-            while True:
-                line = subprocess.stdout.readline().strip()
-                if line.startswith("id name"):
-                    if eng == self.engine1:
-                        self.name1 = line[7:-1].strip()
-                    else:
-                        self.name2 = line[7:-1].strip()
-                elif line == "uaiok": 
-                    break
-
-    def swap_colors(self):
-        temp = self.engine_red
-        self.engine_red = self.engine_blue
-        self.engine_blue = temp
-
-    def terminate_engines(self):
-        self.engine_red.terminate()
-        self.engine_blue.terminate()
-
-    def read_stdout(self, stdout):
-        if self.engine1.stdout != stdout and self.engine2.stdout != stdout:
-            return False
-
-        eng = self.engine1 if self.engine1.stdout == stdout else self.engine2
-        line = eng.stdout.readline().strip()
-
-        if line.startswith("bestmove"):
-            str_move = line.split(" ")[-1].strip()
-            board.makemove(ataxx.Move.from_san(str_move))
-
-            if board.gameover():
-                self.onGameOver()
-                return
-
-            fen = board.get_fen()..replace("x", "r").replace("o", "b").strip()
-            new_color = fen.split(" ")[-3]
-            assert new_color == "r" or new_color == "b"
-            color_that_played = "o" if new_color == "x" else "x"
-
-
-    def onGameOver(self):
-        str_result = board.result().strip()
-        games += 1
-        if str_result == "1-0":
-            w_red += 1
-            l_blue += 1
-            if self.engine1 == self.engine_red:
-                w += 1
-            else:
-                l += 1
-        else if str_result == "0-1":
-            w_blue += 1
-            l_red += 1
-            if self.engine1 == self.engine_red:
-                l += 1
-            else:
-                w += 1
-        else:
-            d += 1
-
-        assert w_red + w_blue == w + l
-        assert w + l + d == games
-        assert w_red + w_blue + l_red + l_blue == games - d
-
-        print("(" + name1 + " vs " + name2 + ")", end="")
-        print(" WLD", w, "-", l, "-", d, "(" + str(games) + ")", end ="")
-        if game_result_type != GAME_RESULT_NORMAL:
-            assert game_result_type == ENGINE1_OUT_OF_TIME or game_result_type == ENGINE2_OUT_OF_TIME
-            print(" Engine 1 " + engine1.name if ENGINE1_OUT_OF_TIME else "Engine 2 " + engine2.name, end="")
-            print(" out of time", end="")
-        print(", red wins " + str(w_red) + ", blue wins " + str(w_blue))
-
-        if games % RATING_INTERVAL == 0:
-            llr = sprt(w, l, d, elo0, elo1, cutechess_sprt)
-            e1, e2, e3 = elo_wld(w, l, d)
-            e1 = int(e1)
-            e2 = int(e2)
-            e3 = int(e3)
-            print(f"ELO: {e2} +- {(e3 - e1) / 2} [{e1}, {e3}]")
-            print(f"LLR: {llr:.3} [{elo0:.3}, {elo1:.3}] ({lower:.3}, {upper:.3})")
-            print("H1 accepted" if llr >= upper else ("H0 accepted" if llr <= lower else "Continue playing"))
-            if llr >= upper or llr <= lower:
-                for engine in engines:
-
-
-
-# Parse args
-parser = argparse.ArgumentParser(description="Run tournament between 2 Ataxx engines")
-parser.add_argument("--engine1", help="Engine 1 exe", type=str, required=True)
-parser.add_argument("--engine2", help="Engine 2 exe", type=str, required=True) 
-parser.add_argument("--concurrency", help="Concurrency", type=int, required=True) 
-args = parser.parse_args()
-print("Concurrency", args.concurrency)
-
-# Load openings
-openingsFile = open("openings_3ply.txt", "r")
-OPENINGS = openingsFile.readlines()
-openingsFile.close()
-assert len(OPENINGS) == 880
-
-# Load games and engines
-games = []
+# Lists for engines and games
 engines = []
-for i in range(args.concurrency):
-    games.append(Game(arg.engine1, arg.engine2))
-    engines.append(games[-1].engine1)
-    engines.append(games[-1].engine2)
-
-# Register the CTRL+C signal handler
-signal.signal(signal.SIGINT, lambda signum, frame: handle_ctrl_c(engines))
+games = []
 
 # Useful vars
-w = l = d = games = w_red = w_blue = l_red = l_blue = 0
+w = l = d = games_finished = w_red = w_blue = l_red = l_blue = 0
 
 # SPRT settings
 alpha = 0.05
@@ -216,55 +32,195 @@ elo1 = 5
 cutechess_sprt = True
 lower = log(beta / (1 - alpha))
 upper = log((1 - beta) / alpha)
-RATING_INTERVAL = 20
+RATING_INTERVAL = 10
 
-# Main loop
-while True:
-    # Use select to efficiently wait for data on stdout streams
-    readable, _, _ = select.select([engine.stdout for engine in engines], [], [])
-    for stdout in readable:
-        for game in games:
-            read_stdout(stdout)
+def quit():
+    print("Terminating engines...")
+    for engine in engines:
+        engine.subprocess.terminate()
+    print("Engines terminated")
+    exit(1)
 
+def handle_ctrl_c():
+    print("Ctrl+C pressed")
+    quit()
 
+# Register the CTRL+C signal handler
+signal.signal(signal.SIGINT, lambda signum, frame: handle_ctrl_c())
 
+# Load openings
+openingsFile = open("openings_3ply.txt", "r")
+OPENINGS = openingsFile.readlines()
+openingsFile.close()
+assert len(OPENINGS) == 880
 
+class Engine:
+    subprocess = None
+    name = None
+    milliseconds = None
 
-    while True:
-        game_result, game_result_type = run_game(engine1, engine2)
-        games += 1
-        if game_result == ENGINE1_WIN:
-            w += 1
-            if engine1.color == RED:
-                assert engine2.color == BLUE
-                w_red += 1
-            else:
-                assert engine1.color == BLUE and engine2.color == RED
-                w_blue += 1
-        elif game_result == ENGINE1_LOSS:
-            l += 1
-            if engine1.color == BLUE:
-                assert engine2.color == RED
-                w_red += 1
-            else:
-                assert engine1.color == RED and engine2.color == BLUE
-                w_blue += 1
+    def __init__(self, exe):
+        self.subprocess = subprocess.Popen(exe, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        assert self.subprocess != None
+
+        # Init engine with "uai" and get engine name
+        self.send("uai")
+        while True:
+            line = self.subprocess.stdout.readline().strip()
+            if line.startswith("id name"):
+                self.name = line[7:-1].strip()
+            elif line == "uaiok": 
+                break
+
+        assert self.name != None and self.name != ""
+
+    def __eq__(self, other):
+        if other == None or other is None:
+            return False
+        assert isinstance(other, Engine)
+        return self.subprocess == other.subprocess
+
+    def send(self, command):
+        self.subprocess.stdin.write(command + "\n")
+        self.subprocess.stdin.flush()
+
+class Game:
+    id = None
+    board = None
+    eng1 = None
+    eng2 = None
+    eng_red = None
+    eng_blue = None
+    eng_to_play = None
+    saved_time = None
+
+    def __init__(self, id, eng1, eng2):
+        self.id = id
+        self.eng1 = eng1
+        self.eng2 = eng2
+        assert self.eng1 != None and self.eng2 != None
+
+    def swap_colors(self):
+        temp = self.eng_red
+        self.eng_red = self.eng_blue
+        self.eng_blue = temp
+
+    def swap_eng_to_play(self):
+        assert self.eng1 != None and self.eng2 != None
+        assert self.eng_red != None and self.eng_blue != None
+        assert self.eng_to_play != None
+
+        self.eng_to_play = self.eng_red if self.eng_to_play == self.eng_blue else self.eng_blue
+
+    def send_go(self):
+        assert self.board != None
+        assert self.eng1 != None and self.eng2 != None
+        assert self.eng_red != None and self.eng_blue != None
+        assert self.eng_to_play != None
+
+        command = "go"
+        command += " rtime " + str(eng_red.milliseconds)
+        command += " btime " + str(eng_blue.milliseconds)
+        command += " rinc " + str(GAME_INCREMENT_MILLISECONDS)
+        command += " binc " + str(GAME_INCREMENT_MILLISECONDS)
+
+        self.saved_time = time.time()
+        self.eng_to_play.send(command)
+
+    def start_new_game(self):
+        assert self.eng1 != None and self.eng2 != None
+
+        fen_opening = OPENINGS[random.randint(0, len(OPENINGS) - 1)].strip()
+        board = ataxx.Board(fen_opening)
+
+        my_fen = fen_opening.replace("x", "r").replace("o", "b").strip()
+        self.eng1.send("position fen " + my_fen)
+        self.eng2.send("position fen " + my_fen)
+
+        # randomly decide engine color
+        if random.randint(0,1) == 0:
+            self.eng_red = self.eng1
+            self.eng_blue = self.eng2
         else:
-            assert game_result == DRAW
+            self.eng_red = self.eng2
+            self.eng_blue = self.eng1
+
+        color = my_fen.split(" ").strip()[-3]
+        assert color == "r" or color == "o"
+        self.eng_to_play = self.eng_red if color == "r" else self.eng_blue
+
+        self.eng1.milliseconds = self.eng2.milliseconds = GAME_MILLISECONDS
+
+    def read_stdout(self, stdout):
+        assert self.board != None
+        assert self.eng1 != None and self.eng2 != None
+        assert self.eng_red != None and self.eng_blue != None
+        assert self.eng_to_play != None
+
+        if stdout == None or stdout.strip() == "":
+            return 
+
+        if self.eng1.subprocess.stdout != stdout and self.eng2.subprocess.stdout != stdout:
+            return 
+
+        assert self.eng_to_play.subprocess.stdout == stdout
+        line = self.eng_to_play.subprocess.stdout.readline().strip()
+
+        if line.startswith("bestmove"):
+            str_move = line.split(" ")[-1].strip()
+            board.makemove(ataxx.Move.from_san(str_move))
+
+            self.eng_to_play.milliseconds -= int((time.time() - self.saved_time) * 1000)
+            if self.eng_to_play.milliseconds <= 0:
+                self.on_game_over(GAME_RESULT_OUT_OF_TIME)
+
+            if board.gameover():
+                self.on_game_over(GAME_RESULT_NORMAL)
+                return
+
+            self.eng1.milliseconds += GAME_INCREMENT_MILLISECONDS
+            self.eng2.milliseconds += GAME_INCREMENT_MILLISECONDS
+
+            fen = self.board.get_fen().replace("x", "r").replace("o", "b").strip()
+            self.eng1.send("position fen " + fen)
+            self.eng2.send("position fen " + fen)
+            self.swap_eng_to_play()
+            self.send_go()
+
+    def on_game_over(self, game_result_type):
+        str_result = board.result().strip()
+        games_finished += 1
+        if str_result == "1-0" or (game_result_type == GAME_RESULT_OUT_OF_TIME and self.eng_to_play == self.engine_blue):
+            w_red += 1
+            l_blue += 1
+            if self.eng1 == self.eng_red:
+                w += 1
+            else:
+                l += 1
+        elif str_result == "0-1" or (game_result_type == GAME_RESULT_OUT_OF_TIME and self.eng_to_play == self.engine_red):
+            w_blue += 1
+            l_red += 1
+            if self.eng1 == self.eng_red:
+                l += 1
+            else:
+                w += 1
+        else:
             d += 1
 
         assert w_red + w_blue == w + l
-        assert w + l + d == games
+        assert w + l + d == games_finsihed
+        assert w_red + w_blue + l_red + l_blue == games_finished - d
 
-        print("(" + engine1.name + " vs " + engine2.name + ")", end="")
-        print(" WLD", w, "-", l, "-", d, "(" + str(games) + ")", end ="")
+        print("(Board " + str(self.id) + ")", end="")
+        print("(" + self.eng1.name + " vs " + self.eng2.name + ")", end="")
+        print(" WLD", w, "-", l, "-", d, "(" + str(games_finished) + ")", end ="")
         if game_result_type != GAME_RESULT_NORMAL:
             assert game_result_type == ENGINE1_OUT_OF_TIME or game_result_type == ENGINE2_OUT_OF_TIME
             print(" Engine 1 " + engine1.name if ENGINE1_OUT_OF_TIME else "Engine 2 " + engine2.name, end="")
             print(" out of time", end="")
         print(", red wins " + str(w_red) + ", blue wins " + str(w_blue))
 
-        if games % RATING_INTERVAL == 0:
+        if games_finished % RATING_INTERVAL == 0:
             llr = sprt(w, l, d, elo0, elo1, cutechess_sprt)
             e1, e2, e3 = elo_wld(w, l, d)
             e1 = int(e1)
@@ -274,8 +230,49 @@ while True:
             print(f"LLR: {llr:.3} [{elo0:.3}, {elo1:.3}] ({lower:.3}, {upper:.3})")
             print("H1 accepted" if llr >= upper else ("H0 accepted" if llr <= lower else "Continue playing"))
             if llr >= upper or llr <= lower:
-                break
+                quit()
 
-    print("Terminating engines...")
-    engine1.terminate()
-    engine2.terminate()
+        self.start_new_game()
+
+# Parse args
+parser = argparse.ArgumentParser(description="Run tournament between 2 Ataxx engines")
+parser.add_argument("--engine1", help="Engine 1 exe", type=str, required=True)
+parser.add_argument("--engine2", help="Engine 2 exe", type=str, required=True) 
+parser.add_argument("--concurrency", help="Concurrency", type=int, required=True) 
+args = parser.parse_args()
+print("Concurrency", args.concurrency)
+print(args.engine1, "vs", args.engine2)
+
+# Create a selector
+selector = selectors.DefaultSelector()
+
+# Load engines and games
+for i in range(args.concurrency):
+    eng1 = Engine(args.engine1)
+    eng2 = Engine(args.engine2)
+    engines.append(eng1)
+    engines.append(eng2)
+    games.append(Game(i+1, eng1, eng2))
+
+subprocess_list = [engine.subprocess for engine in engines]
+
+# Create a dictionary to store the subprocesses' output buffers
+output_buffers = {process.stdout: [] for process in subprocess_list}
+
+# Create a dictionary to store the subprocesses' input buffers
+input_buffers = {process.stdin: [] for process in subprocess_list}
+
+# Main loop
+while any(process.poll() is None for process in subprocess_list):
+    # Check for subprocesses with available data to read
+    readable, _, _ = select.select(output_buffers.keys(), [], [], 0.1)
+
+    # Read from readable subprocesses
+    for stream in readable:
+        process = next(process for process, stdout in output_buffers.items() if stdout is stream)
+        line = stream.readline().strip()
+        if line:
+            print(f"Received from process {process.pid}: {line}")
+
+
+quit()
