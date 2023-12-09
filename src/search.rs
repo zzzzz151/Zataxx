b01baf00f6f424128f7aa178acaecd50ecfb7f4a
zzzzz <ricardopinto15@hotmail.com>
2023-12-09 01:33:26 +0000

    LMP

--- src/search.rs
+++ src/search.rs
@@ -121,6 +121,11 @@ fn pvs(search_data: &mut SearchData, mut depth: i16, ply: i16, mut alpha: i16, b
 
         if ply > 0 && best_score > -MIN_WIN_SCORE && move_score <= 1
         {
+            // LMP (Late mogve pruning
+            if i as i16 >= 3 + pv_node as i16 + depth * depth / 2 {
+                break;
+            }
+
             // FP (Futility pruning)
             if depth <= 5 && alpha < MIN_WIN_SCORE
             && eval + 140 + depth * 75 <= alpha {
